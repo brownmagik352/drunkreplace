8942b5b8ebf333acd6b7594b221b04e3b6eb155b
pmaddi <pranav.maddi@gmail.com>
2013-11-09 12:53:00 -0500

    Made this shit work. No mo errorz.

--- drunkreplace.py
+++ drunkreplace.py
@@ -4,16 +4,28 @@ import cv2, math
 import numpy as np
 import sys
 
+import Image
+import StringIO
+import boto
+import os
+import random, string
+from boto.s3.key import Key
+
 ### Constants
 LowRedlow = 0
 LowRedhigh = 5
 UpRedlow = 175
 UpRedhigh = 180
 ExclusionRatio = 10
+Offset = 0
 
 ###
 
 ### Functions
+
+# def sendImage(image):
+	
+
 def not_min_size(area, height, width):
 	if area >= height*width / ExclusionRatio:
 		return 0
@@ -31,27 +43,37 @@ def max_min_box(box):
 
 def replace_contour(largest_contour, img):
 	## Find the box excompassing the largest red blob
+	pheight, pwidth, pdepth = img.shape
+
 	rect = cv2.minAreaRect(largest_contour)
 	box = cv2.cv.BoxPoints(rect)
 	box = np.int0(box)
 	# print box
-	box_width = box[3][0]-box[1][0]
-	box[3][1]=(1.2*box_width)+box[1][1]
-	box_height = box[3][1]-box[1][1]
+	[minx,maxx,miny,maxy] = max_min_box(box)
+
+	box_width = maxx-minx
+	maxy = int((1.2*box_width)+miny)
+	if maxy > pheight:
+		maxy = pheight-1
+
+	box_height = maxy-miny
+
 	replace = cv2.imread('coke1.png')
 	rheight, rwidth, rdepth = replace.shape
-	replace_resize = cv2.resize(replace, (box_width , box_height))
+	replace_resize = cv2.resize(replace, (int(box_width) , int(box_height)))
 	theight, twidth, tdepth = replace_resize.shape
 	replace_transform = cv2.cvtColor(replace_resize, cv2.COLOR_BGR2HSV)
 
 	## Replace pixels in blob
-	s,w,z = img.shape
-	for x in range (int(box[1][0]), int(box[3][0])):
-	 	for y in range (int(box[1][1]), int(box[3][1])):
-	 		if x > 0 and x < s and y > 0 and x < w :
-				px = img[y][x]
-		 		if (px[0] >= 0 and px[1] >= 100 and px[2] >= 0 and px[0] <= 10 and px[1] <= 255 and px[2] <= 255) or (px[0] >= 170 and px[1] >= 100 and px[2] >= 0 and px[0] <= 180 and px[1] <= 255 and px[2] <= 255): 
-					img[y][x] = replace_transform[(theight/2)+((y-box[1][1])-(theight/2))][(twidth/2)+((x-box[1][0])-(twidth/2))]
+	# s,w,z = img.shape
+	# [xmin,xmax,ymin,ymax] = max_min_box(box)
+	for x in range (minx, maxx):
+	 	for y in range (miny, maxy):
+	 		# if y > 0 and y < s and x > 0 and x < w :
+	 			# print img.shape, y, x
+			px = img[y][x]
+	 		if (px[0] >= LowRedlow and px[1] >= 100 and px[2] >= 0 and px[0] <= LowRedhigh-Offset and px[1] <= 255 and px[2] <= 255) or (px[0] >= UpRedlow+Offset and px[1] >= 100 and px[2] >= 0 and px[0] <= UpRedhigh and px[1] <= 255 and px[2] <= 255): 
+				img[y][x] = replace_transform[(theight/2)+((y-miny)-(theight/2))][(twidth/2)+((x-minx)-(twidth/2))]
 	return img
 
 def draw_box(largest_contour, img):
@@ -59,14 +81,6 @@ def draw_box(largest_contour, img):
 	rect = cv2.minAreaRect(largest_contour)
 	box = cv2.cv.BoxPoints(rect)
 	box = np.int0(box)
-	box_width = box[3][0]-box[1][0]
-	box[3][1]=(1.2*box_width)+box[1][1]
-	box_height = box[3][1]-box[1][1]
-	replace = cv2.imread('coke1.png')
-	rheight, rwidth, rdepth = replace.shape
-	replace_resize = cv2.resize(replace, (box_width , box_height))
-	theight, twidth, tdepth = replace_resize.shape
-	replace_transform = cv2.cvtColor(replace_resize, cv2.COLOR_BGR2HSV)
 	box = np.int0(box)
 	cv2.drawContours(img,[box], 0, (0, 0, 255), 2)
 	return img
@@ -140,11 +154,11 @@ for x in xrange(0,len(contours)):
 	cv2.drawContours(orig_img, contoursSorted, x, [x*10,0,100],thickness = 5)
 cv2.imwrite('contours.png',orig_img)
 
-# a = img
-# for x in xrange(0,len(contours)):
-# 	a = draw_box(contoursSorted[x], a)
-# a = cv2.cvtColor(a, cv2.COLOR_HSV2BGR)
-# cv2.imwrite('boxes.png',a)
+a = img
+for x in xrange(0,len(contours)):
+	a = draw_box(contoursSorted[x], a)
+a = cv2.cvtColor(a, cv2.COLOR_HSV2BGR)
+cv2.imwrite('boxes.png',a)
 
 for x in xrange(0,len(contours)):
 	img = replace_contour(contoursSorted[x], img)
